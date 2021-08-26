3a02eea25fa62937602b2575166fa4f3cd5adf81
jdesgarennes <johndesgarennes@gmail.com>
2021-08-26 15:22:07 -0700

    Added functions for array random types and they test good.

--- Develop/script.js
+++ Develop/script.js
@@ -4,26 +4,28 @@ var generateBtn = document.querySelector("#generate");
 // Define all the possible array types like letter numbers and special characters.
 
 //All lower case character letters with a console log checking them
-var lowerCasedCharacters =[
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-console.log(lowerCasedCharacters[2]);
+var lowerCasedLetters =[
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//console.log(lowerCasedLetters[2]);
 
 //All Upper case character letters with a console log checking them
 
-var upperCasedCharacters =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-console.log(upperCasedCharacters[2]);
+var upperCasedLetters =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//console.log(upperCasedLetters[2]);
 
 //All numbers with a console log checking them
 
 var allNumbers =['0','1','2','3','4','5','6','7','8','9'];
-console.log(allNumbers[2]);
+//console.log(allNumbers[2]);
 
 //All Special characters with a console log checking them
 
-var specialCharaters = ['!','\"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
+var specialCharaters = ['!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'];
 // Check to make sure escape sequences work :)
-console.log(specialCharaters[1],specialCharaters[6], specialCharaters[23]);
+//console.log(specialCharaters[1],specialCharaters[6], specialCharaters[23]);
 
+var possibleCharacters = [];
+var finalPassword = [];
 
 // Create a prompt to ask how many characters in the password you would like.
 
@@ -47,11 +49,12 @@ console.log(specialCharaters[1],specialCharaters[6], specialCharaters[23]);
   // need to alert
 //  ("Please reenter with valid paramenters. May need to reload the page.");
 //}
+// My final password array
 
 
 var options = {
 
-  length: length,
+  length: 12,
   hasLowercase: true,
   hasNumbers: true,
   hasUpperCase: true,
@@ -60,14 +63,16 @@ var options = {
 }
 // create function for getting random
 
-function getRandomChar(array) { 
-  return array[Math.floor(math.floor*array.length)]
+function getRandomStuff(array) { 
+  return array[Math.floor(Math.random() *array.length)];
 
 }
 
 
 // Create a generate function password
-var possibleCharacters = [];
+
+var finalPassword =[];
+
 function generatePassword(){
   // need gaurentee of at least one type selected
   //need an array for final password
@@ -78,14 +83,25 @@ function generatePassword(){
 
   if (options.hasSpecialCharacters){
     possibleCharacters = possibleCharacters.concat(specialCharaters);
-    getRandomChar(specialCharaters)
+    finalPassword.push(getRandomStuff(specialCharaters));
   }
-
+  if (options.hasUpperCase){
+    possibleCharacters = possibleCharacters.concat(upperCasedLetters);
+    finalPassword.push(getRandomStuff(upperCasedLetters));
+  }
+  if (options.hasNumbers){
+    possibleCharacters = possibleCharacters.concat(allNumbers);
+    finalPassword.push(getRandomStuff(allNumbers));
+  }
+  if (options.hasLowercase){
+    possibleCharacters = possibleCharacters.concat(lowerCasedLetters);
+    finalPassword.push(getRandomStuff(lowerCasedLetters));
+    console.log(finalPassword);
+  }
 
 }
 
 generatePassword();
-
 
 // Write password to the #password input
 function writePassword() {
